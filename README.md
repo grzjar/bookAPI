7b96d0c72093ce5d50e2e56cde875d12eb9d9160
grzegorzjaroszewski <jaroszewski.g@gmail.com>
2021-06-12 14:08:29 +0200

    Readme v1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # bookAPI
+
+Aplikacja do zarządzania książkami w bazie danych w oparciu o Hibernate'a i Spring Boota.
+Wykorzystuje też bazę danych MySQL do przechowywania informacji o książkach.
+
+Możliwe są następujące akcje:
--- src/main/java/pl/grzjar/bookAPI/config/DataSetup.java
+++ src/main/java/pl/grzjar/bookAPI/config/DataSetup.java
@@ -1,35 +1,35 @@
-package pl.grzjar.bookAPI.config;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import pl.grzjar.bookAPI.model.Book;
-import pl.grzjar.bookAPI.repository.BookRepository;
-
-import java.util.concurrent.atomic.AtomicBoolean;
-
-@Component
-@Slf4j
-public class DataSetup {
-
-    private AtomicBoolean alreadyRun = new AtomicBoolean(false);
-
-    private final BookRepository bookRepository;
-
-    public DataSetup(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
-    @EventListener
-    @Transactional
-    public void testData(ContextRefreshedEvent event) {
-        if (!alreadyRun.getAndSet(true)) {
-            Book book = new Book(null, "12211", "JAVA", "Ja", "Też ja", "online");
-            bookRepository.save(book);
-            Book book1 = new Book(null, "199765756475", "Python", "Nie ja", "Również nie ja", "nie istnieje");
-            bookRepository.save(book1);
-        }
-    }
-}
+//package pl.grzjar.bookAPI.config;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.event.ContextRefreshedEvent;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//import pl.grzjar.bookAPI.model.Book;
+//import pl.grzjar.bookAPI.repository.BookRepository;
+//
+//import java.util.concurrent.atomic.AtomicBoolean;
+//
+//@Component
+//@Slf4j
+//public class DataSetup {
+//
+//    private AtomicBoolean alreadyRun = new AtomicBoolean(false);
+//
+//    private final BookRepository bookRepository;
+//
+//    public DataSetup(BookRepository bookRepository) {
+//        this.bookRepository = bookRepository;
+//    }
+//
+//    @EventListener
+//    @Transactional
+//    public void testData(ContextRefreshedEvent event) {
+//        if (!alreadyRun.getAndSet(true)) {
+//            Book book = new Book(null, "12211", "JAVA", "Ja", "Też ja", "online");
+//            bookRepository.save(book);
+//            Book book1 = new Book(null, "199765756475", "Python", "Nie ja", "Również nie ja", "nie istnieje");
+//            bookRepository.save(book1);
+//        }
+//    }
+//}
